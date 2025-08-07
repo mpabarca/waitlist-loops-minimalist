b1af0bcc59ae6d5a9db2f33300ae6023bac36186
mpabarca <mp.abarca.alvear@gmail.com>
2025-08-07 11:47:33 +0200

    Make EarlyAccessBadge responsive

--- src/components/EarlyAccessBadge.tsx
+++ src/components/EarlyAccessBadge.tsx
@@ -1,6 +1,6 @@
 function EarlyAccessBadge() {
   return (
-    <div className="bg-gray-100 text-gray-600 text-xs py-1.5 px-4 rounded-md uppercase ">
+    <div className="bg-gray-100 text-gray-600 text-[10px] md:text-xs py-1.5 px-4 rounded-md uppercase ">
       In Early Access Now
     </div>
   );
