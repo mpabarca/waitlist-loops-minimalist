23952da2c550aa96b638a900bccb155497a9e263
mpabarca <mp.abarca.alvear@gmail.com>
2025-08-07 11:43:09 +0200

    Create EarlyAccessBadge component

---
+++ src/components/EarlyAccessBadge.tsx
@@ -0,0 +1,9 @@
+function EarlyAccessBadge() {
+  return (
+    <div className="bg-gray-100 text-gray-600 text-xs py-1.5 px-4 rounded-md uppercase ">
+      In Early Access Now
+    </div>
+  );
+};
+
+export default EarlyAccessBadge;
