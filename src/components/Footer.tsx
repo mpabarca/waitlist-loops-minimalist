e43c04ab63c878442138a1f2b1db3c8aaad096f3
mpabarca <mp.abarca.alvear@gmail.com>
2025-08-07 20:25:45 +0200

    Add Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer className="w-full text-center text-gray-500 text-sm font-light py-4">
+      © 2025 Crafted by{" "}
+      <a
+        href='https://mpabarca.dev/'
+        target='_blank'
+        rel='noopener'
+        className='underline'
+      >
+        Maria Paz Abarca
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,22 +1,30 @@
 import EarlyAccessBadge from "@/components/EarlyAccessBadge";
+import Footer from "@/components/Footer";
 import LoopsForm from "@/components/LoopsForm";
 
 export default function LandingPage() {
   return (
-    <main className='min-h-screen flex flex-col items-center justify-center mx-4 md:mx-auto text-center gap-8 pb-14'>
-      <section className='flex flex-col gap-4 items-center'>
-        {/* Logo */}
-        <img src='/vite.svg' alt='Logo' className='w-12 h-12 md:w-15 md:h-15' />
-        <EarlyAccessBadge />
-      </section>
-      <section className='flex flex-col gap-4'>
-        <h1 className='text-4xl font-bold'>Launch with Confidence</h1>
-        <p className='max-w-md'>
-          Collect emails, build hype, and validate your idea with a beautiful
-          pre-launch page.
-        </p>
-      </section>
-      <LoopsForm />
+    <main className='min-h-screen flex flex-col items-center justify-center mx-4 md:mx-auto'>
+      <div className='flex flex-col justify-center items-center text-center gap-8 my-auto pb-14'>
+        <section className='flex flex-col gap-4 items-center'>
+          {/* Logo */}
+          <img
+            src='/vite.svg'
+            alt='Logo'
+            className='w-12 h-12 md:w-15 md:h-15'
+          />
+          <EarlyAccessBadge />
+        </section>
+        <section className='flex flex-col gap-4'>
+          <h1 className='text-4xl font-bold'>Launch with Confidence</h1>
+          <p className='max-w-md'>
+            Collect emails, build hype, and validate your idea with a beautiful
+            pre-launch page.
+          </p>
+        </section>
+        <LoopsForm />
+      </div>
+      <Footer />
     </main>
   );
 }
