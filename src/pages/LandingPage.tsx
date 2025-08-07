db35f9c4f15eb5be0dc22531387a89751cb94aa4
mpabarca <mp.abarca.alvear@gmail.com>
2025-08-07 11:46:07 +0200

    Rearrange LandingPage & Make it responsive

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,14 +1,21 @@
+import EarlyAccessBadge from "@/components/EarlyAccessBadge";
 import LoopsForm from "@/components/LoopsForm";
 
 export default function LandingPage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center px-6 text-center">
-      <img src="/vite.svg" alt="Logo" className="w-20 h-20 mb-6" />
-      <h1 className="text-4xl font-bold mb-4">Launch with Confidence</h1>
-      <p className="mb-8 max-w-md">
-        Collect emails, build hype, and validate your idea with a beautiful pre-launch page.
-      </p>
+    <main className='min-h-screen flex flex-col items-center justify-center mx-4 md:mx-auto text-center gap-8 pb-14'>
+      <section className='flex flex-col gap-4 items-center'>
+        <img src='/vite.svg' alt='Logo' className='w-12 h-12 md:w-15 md:h-15' />
+        <EarlyAccessBadge />
+      </section>
+      <section className='flex flex-col gap-4'>
+        <h1 className='text-4xl font-bold'>Launch with Confidence</h1>
+        <p className='max-w-md'>
+          Collect emails, build hype, and validate your idea with a beautiful
+          pre-launch page.
+        </p>
+      </section>
       <LoopsForm />
-    </div>
+    </main>
   );
 }
