446bdaa1928c7cc80eaf82bcf59455d9db628826
mpabarca <mp.abarca.alvear@gmail.com>
2025-08-16 10:14:43 +0200

    Add variables to custom style on EarlyAccessBadge component

--- src/components/EarlyAccessBadge.tsx
+++ src/components/EarlyAccessBadge.tsx
@@ -1,9 +1,15 @@
 function EarlyAccessBadge() {
+  const background = "bg-gray-100";
+  const fontColor = "text-gray-600";
+  const borderColor = "rounded-md";
+
   return (
-    <div className="bg-gray-100 text-gray-600 text-[10px] md:text-xs py-1.5 px-4 rounded-md uppercase ">
+    <div
+      className={`${background} ${fontColor} ${borderColor} text-[10px] md:text-xs pt-2 pb-1.5 px-4 uppercase`}
+    >
       In Early Access Now
     </div>
   );
-};
+}
 
 export default EarlyAccessBadge;
