f22098c2f6515be612c957acd707c2108350093e
mpabarca <mp.abarca.alvear@gmail.com>
2025-08-16 10:14:18 +0200

    Separate sections on landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,18 +3,17 @@ import Footer from "@/components/Footer";
 import LoopsForm from "@/components/LoopsForm";
 
 export default function LandingPage() {
+  const sizeLogo = "w-12 h-12 md:w-15 md:h-15";
+
   return (
     <main className='min-h-screen flex flex-col items-center justify-center mx-4 md:mx-auto'>
       <div className='flex flex-col justify-center items-center text-center gap-8 my-auto pb-14'>
+        {/* Logo + Badge */}
         <section className='flex flex-col gap-4 items-center'>
-          {/* Logo */}
-          <img
-            src='/vite.svg'
-            alt='Logo'
-            className='w-12 h-12 md:w-15 md:h-15'
-          />
+          <img src='/vite.svg' alt='Logo' className={sizeLogo} />
           <EarlyAccessBadge />
         </section>
+        {/* Title & Subtitle */}
         <section className='flex flex-col gap-4'>
           <h1 className='text-4xl font-bold'>Launch with Confidence</h1>
           <p className='max-w-md'>
@@ -22,7 +21,10 @@ export default function LandingPage() {
             pre-launch page.
           </p>
         </section>
-        <LoopsForm />
+        {/* Form */}
+        <section className='h-32 w-full flex flex-col justify-center'>
+          <LoopsForm />
+        </section>
       </div>
       <Footer />
     </main>
